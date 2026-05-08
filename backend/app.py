776c5aa474778502033e6b9d66460560142e4f4d
Dónovan Alonso <up210762@alumnos.upa.edu.mx>
2026-05-08 04:11:31 -0600

    Primer versión Docker

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,7 @@ try:
     app = init_app(configuration)
     if __name__ == '__main__':
         app.run(debug=True,
+                host="0.0.0.0",
                 port=5000)
         
 except Exception as ex:
