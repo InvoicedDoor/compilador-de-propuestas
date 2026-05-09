6b2ce5e54e4cea13e09b9157457457017bbb633f
Donovan Hernández <up210762@alumnos.upa.edu.mx>
2026-05-08 18:29:33 -0600

    Corrección de Docker

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 from src import init_app
 from config import config
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv()
+
+log_directory = os.getenv("DEFAULT_LOGGER_DIRECTORY")
+log_file = os.getenv("DEFAULT_LOGGER_FILENAME")
 
 try:
     configuration = config['development']
+    if not Path.exists(f"{log_directory}/{log_file}"):
+        Path.mkdir(f"{log_directory}/{log_file}")
     app = init_app(configuration)
     if __name__ == '__main__':
-        app.run(debug=True,
+        app.run(debug=False,
                 host="0.0.0.0",
                 port=5000)
         
